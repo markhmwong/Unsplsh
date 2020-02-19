74b97f94c620a0962d6eb30a8f45cf510e3bd42f
markhmwong <markhmwong@gmail.com>
2020-02-19 15:16:31 +1100

    traditional datasource conversion complete

--- Unsplsh/MainViewController/MainViewController.swift
+++ Unsplsh/MainViewController/MainViewController.swift
@@ -14,6 +14,7 @@ class MainViewController: UIViewController {
 		let view = UITableView(frame: .zero)
 		view.translatesAutoresizingMaskIntoConstraints = false
 		view.delegate = self
+		view.dataSource = self
 		view.estimatedRowHeight = 350.0
 		view.rowHeight = UITableView.automaticDimension
 		view.separatorStyle = .none
@@ -41,9 +42,18 @@ class MainViewController: UIViewController {
 		view.addSubview(tableView)
 		_vm.registerCellsFor(tableView)
 		tableView.anchorView(top: view.topAnchor, bottom: view.bottomAnchor, leading: view.leadingAnchor, trailing: view.trailingAnchor, centerY: nil, centerX: nil, padding: .zero, size: .zero)
+		
 		let apiSession = ApiSession()
 		apiSession.beginRequestForLatestImages { (photos) in
-			_vm.preparePhotos(data: photos)
+			for photo in photos {
+				if let url = URL(string: photo.urls.regular) {
+					_vm.photos?.append(PhotoRecord(name: photo.user.name, url: url, bio:photo.user.bio ?? "Unknown"))
+				}
+			}
+			
+			DispatchQueue.main.async {
+				self.tableView.reloadData()
+			}
 		}
 	}
 
@@ -52,7 +62,25 @@ class MainViewController: UIViewController {
 	}
 }
 
-extension MainViewController: UITableViewDelegate {
+extension MainViewController: UITableViewDataSource, UITableViewDelegate {
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		guard let _vm = viewModel else {
+			tableView.setEmptyMessage("No images were found! Please check internet connection")
+			return 0
+		}
+		return _vm.numberOfRowsFor(tableView)
+	}
+	
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		guard let _vm = viewModel else {
+			// manual creation of cell to fallback
+			let cell = tableView.dequeueReusableCell(withIdentifier: "UnsplshCellId", for: indexPath) as! ImageCell
+			return cell
+		}
+		// standard flow
+		return _vm.tableForCell(tableView, indexPath: indexPath)
+	}
+	
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
 		guard let _vm = viewModel else {
 			let height = UITableView.automaticDimension
